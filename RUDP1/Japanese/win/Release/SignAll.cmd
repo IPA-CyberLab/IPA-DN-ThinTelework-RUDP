7f87ec08dd710d951d2116e9bc5292a1d7b08ff1
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2020-04-15 12:31:28 +0900

    RUDP1 Initial Release

---
+++ RUDP1/Japanese/win/Release/SignAll.cmd
@@ -0,0 +1,12 @@
+
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\git\IPA-DN-ThinTelework-RUDP\RUDP1\Japanese\win\Release\logmessages.dll
+
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\git\IPA-DN-ThinTelework-RUDP\RUDP1\Japanese\win\Release\urdpclient.exe
+
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\git\IPA-DN-ThinTelework-RUDP\RUDP1\Japanese\win\Release\urdpconfig.exe
+
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\git\IPA-DN-ThinTelework-RUDP\RUDP1\Japanese\win\Release\urdpserver.exe
+
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\git\IPA-DN-ThinTelework-RUDP\RUDP1\Japanese\win\Release\wm_hooks.dll
+
+
